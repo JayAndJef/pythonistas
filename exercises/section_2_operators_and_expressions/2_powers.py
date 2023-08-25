4a4b5e492cc426c8605001d85019b535b4ed3648
uhmmer <83677670+uhmmer@users.noreply.github.com>
2023-08-25 16:36:20 -0400

    Update 2_powers.py

---
+++ exercises/section_2_operators_and_expressions/2_powers.py
@@ -0,0 +1,8 @@
+a = 3
+b = 2
+
+#edit between these lines to make the test pass
+mult_operator = a ?? b
+#edit between these lines to make the test pass
+def test_mult_operator():
+  assert mult_operator == 9
