fdac9fc046fbbb968d55e9e9256070babab8a6b8
uhmmer <83677670+uhmmer@users.noreply.github.com>
2023-08-25 16:02:07 -0400

    Update 1_basic_operators.py

--- exercises/section_2_operators_and_expressions/1_basic_operators.py
+++ exercises/section_2_operators_and_expressions/1_basic_operators.py
@@ -1,7 +1,8 @@
 #Edit between these lines to make the test pass
 a = 5
-b = 0
-c = a / b
-
-print(a - b + c)
+b = 2
+div_operator = a ??? b
 #Edit between these lines to make the test pass
+
+def test_div_operator():
+  assert div_operator == 2.5
